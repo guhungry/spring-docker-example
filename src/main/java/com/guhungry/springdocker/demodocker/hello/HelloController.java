a817cf5b4dad7a0c99b062b70bb717baa54a57e7
Woraphot Chokratanasombat <woraphot.c@kbtg.tech>
2019-09-18 11:06:25 +0700

    [Loggin] Change: Add logback

--- src/main/java/com/guhungry/springdocker/demodocker/hello/HelloController.java
+++ src/main/java/com/guhungry/springdocker/demodocker/hello/HelloController.java
@@ -2,6 +2,8 @@ package com.guhungry.springdocker.demodocker.hello;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private HelloService service;
 
     @GetMapping("/ping")
     public String ping() {
+        logger.info("Ping pong game");
         return service.getPong();
     }
 
@@ -22,7 +27,7 @@ public class HelloController {
 
     @GetMapping("/hi/{name}")
     public String hi(@PathVariable String name) {
-
+        logger.info("Say hi to " + name);
         // Metric name = tracking.user
         Counter counter = meterRegistry.counter("tracking.user", "username", name);
         counter.increment();
